9b300094bc7a365f9e4b80695a46d07e390eced1
Julieagarb <162235167+Julieagarb@users.noreply.github.com>
2024-12-03 21:33:23 -0400

    continuaçaowebjs

---
+++ assets/JS/script.js
@@ -0,0 +1,30 @@
+function ocultarMostrar() {
+    var texto = document.getElementById("texto");
+    
+    if (texto.style.display === "none") {
+      texto.style.display = "block";
+    } else {
+      texto.style.display = "none";
+    }
+  }
+
+
+  function ocultarMostrar2() {
+    var texto2 = document.getElementById("texto2");
+    
+    if (texto2.style.display === "none") {
+      texto2.style.display = "block";
+    } else {
+      texto2.style.display = "none";
+    }
+  }
+
+  function ocultarMostrar3() {
+    var texto3 = document.getElementById("texto3");
+    
+    if (texto3.style.display === "none") {
+      texto3.style.display = "block";
+    } else {
+      texto3.style.display = "none";
+    }
+  }
